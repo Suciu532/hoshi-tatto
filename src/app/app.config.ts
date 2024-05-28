664e913fab62783ce5800b44375e050272a1be96
Suciu Tudor <suciu532@gmail.com>
2024-05-28 11:58:49 +0300

    create home, nav component and create routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+// Since Angular 17 uses a functional approach, we declare the components directly.
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      CommonModule 
+    )
+  ]
 };
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },  // Route for Home component
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }  // Wildcard route to redirect any unknown paths to Home
+  ];
